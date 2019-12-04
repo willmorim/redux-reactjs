9d1b742ca133bdf3b8e35612eb4aab9f97ec8c26
willmorim <pdbawilliam@gmail.com>
2019-12-03 21:09:39 -0300

    estilos globais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
+import GlobalStyle from './styles/globals'
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <Routes />
     </BrowserRouter>
   )
