b6f347d242cfdb2356f294bb59035ddabf351bde
willmorim <pdbawilliam@gmail.com>
2020-02-01 17:51:27 -0300

    Configurando Redux Saga

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,9 +1,17 @@
-export function addToCart(product) {
+export function addToCartRequest(id) {
   return {
-    type: '@cart/ADD',
+    type: '@cart/ADD_REQUEST',
+    id
+  }
+}
+
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_SUCCESS',
     product
   }
 }
+
 
 export function removeFromCart(id) {
   return {
